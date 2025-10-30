3d16664339e7977d27e6f5a33312a7def31dbe13
victorcarvalhoad <victor.carvalhoad@gmail.com>
2025-10-30 00:10:06 -0300

    initialize Django project structure with basic settings and app configurations

---
+++ src/config/urls.py
@@ -0,0 +1,18 @@
+"""
+Path -> define o caminho de url e qual função ou view que deve ser executada.
+include -> função que vai incluir configurações de outros arquivos de url. Na intenção de dividir
+           o roteamento em vários arquivos menores, organizando e deixando mais semântico o código.
+django.views.generic -> importa a view genérica do django para exibir um template html.
+"""
+
+from django.contrib import admin
+from django.urls import path, include
+from django.views.generic import TemplateView
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', TemplateView.as_view(template_name='home.html'), name='home'),
+    path('accounts/', include('accounts.urls')),
+    path('game/', include('game.urls')),
+    path('wallet/', include('wallet.urls')),
+]
